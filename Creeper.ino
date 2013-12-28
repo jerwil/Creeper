12f46964ca74cf6b701dbce8b5fea82334951409
Jeremy Wilson <jerwil@gmail.com>
2013-12-28 17:28:31 -0600

    Got fading to work with simpler code

--- Creeper.ino
+++ Creeper.ino
@@ -10,7 +10,6 @@
 int brightness = 0;    // how bright the LED is
 int brightness2 = 255;
 int fadeAmount = 1;    // how many points to fade the LED by
-int fadeAmount2 = 1;
 int count = 0;
 
 int LED1_Pin = 0;
@@ -18,8 +17,7 @@ int LED2_Pin = 1;
 int potPin = 3;
 int pot_val = 0;
 
-int LED1_direction = 1; // Positive means fading in, negative means fading out
-int LED2_direction = 1; // Positive means fading in, negative means fading out
+int LED_direction = 1; // Positive means fading in, negative means fading out
 
 void setup()  { 
   // declare pin 9 to be an output:
@@ -29,35 +27,21 @@ void setup()  {
 
 void loop()  { 
   // set the brightness of pin 9:
-  analogWrite(0, brightness);
-  analogWrite(1, brightness2);
+  analogWrite(LED1_Pin, brightness);
+  analogWrite(LED2_Pin, brightness2);
   pot_val = analogRead(potPin);
   
-  fadeAmount = 5*pot_val/1024*LED1_direction;
-  fadeAmount2 = 5*pot_val/1024*LED2_direction;
-
+  fadeAmount = 15*pot_val/1024*LED_direction;
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
-  brightness2 = brightness2 - fadeAmount;
+  brightness2 = 255 - brightness;
 
 
   // reverse the direction of the fading at the ends of the fade: 
-    if (brightness2 <= 0 || brightness2 >= 255) {
-    LED1_direction = LED1_direction*(-1);  
-    }
-    else {LED1_direction = 1;}
-  
     if (brightness <= 0 || brightness >= 255) {
-    LED2_direction = LED2_direction*(-1);
+    LED_direction = LED_direction*(-1);  
     }
-    else {LED2_direction = 1;}
   
-//      if (count == 255) {
-//    count = 0; 
-//    fadeAmount = -fadeAmount ;
-//  }
-  
-  // wait for 30 milliseconds to see the dimming effect    
   delay(10);                            
 }
